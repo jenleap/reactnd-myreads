c8ccd578023148d3104af6a238b6327aba44ef4c
jenleap <jenleap@gmail.com>
2017-08-18 13:37:23 -0400

    Added links

--- src/ShelfPage.js
+++ src/ShelfPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 import Booklist from './Booklist'
 
@@ -124,7 +125,7 @@ export default class ShelfPage extends Component {
               </div>
             </div>
             <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+              <Link to="/search">Add a book</Link>
             </div>
           </div>
         )
