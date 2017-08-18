da0ac0624d445e47e1a1d797ef46ce5289deb987
jenleap <jenleap@gmail.com>
2017-08-18 13:41:11 -0400

    Added search functionality

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,29 +1,34 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
 
 import Booklist from './Booklist'
 
 export default class SearchPage extends Component {
+
+    state = {
+      searchResults: []
+    }
+
+    search(e) {
+    BooksAPI.search(e, 20).then((books) => {
+            this.setState({ searchResults: books })
+        })
+    }
+
     render() {
         return (
             <div className="search-books">
             <div className="search-books-bar">
                 <Link className="close-search" to="/">Close</Link>
                 <div className="search-books-input-wrapper">
-                {/* 
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-                  
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-                
+                <input type="text" 
+                  placeholder="Search by title or author"
+                  onChange={(event) => this.search(event.target.value)}/>
                 </div>
             </div>
             <div className="search-books-results">
-              <Booklist />
+              <Booklist list={this.state.searchResults} />
             </div>
           </div>
         )
